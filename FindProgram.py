09e52240978dd1967768d076d2a01ca57b0b4b73
ΛLIRΞZΛ <72320688+alyrezo@users.noreply.github.com>
2022-07-03 11:12:47 +0430

    Add files via upload

---
+++ FindProgram.py
@@ -0,0 +1,72 @@
+from googlesearch import search
+import time, os, argparse, requests, colorama , sys
+
+colorama.init()
+
+banner = colorama.Fore.RED + """
+        ███████╗██╗███╗   ██╗██████╗   ██████╗ ██████╗ ██████╗ 
+        ██╔════╝██║████╗  ██║██╔══██╗  ██╔══██╗██╔══██╗██╔══██╗
+        █████╗  ██║██╔██╗ ██║██║  ██║  ██████╔╝██████╔╝██████╔╝
+        ██╔══╝  ██║██║╚██╗██║██║  ██║  ██╔══██╗██╔══██╗██╔═══╝ 
+        ██║     ██║██║ ╚████║██████╔╝  ██████╔╝██████╔╝██║     
+        ╚═╝     ╚═╝╚═╝  ╚═══╝╚═════╝   ╚═════╝ ╚═════╝ ╚═╝     
+
+        """ + colorama.Fore.CYAN + """Twitter:  @Alyrezo
+        Github:  https://github.com/Alyrezo
+        """
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--silence',action="store_const",const=True,help="Not displaying the banner")
+parser.add_argument('-d','--dork',type=str,help="for using your custom dork")
+parser.add_argument('-o','--output',type=str,help="result export as txt file")
+parser.add_argument('-c','--count',type=int,help='count of domains it finds')
+parser.add_argument('-t','--tld',type=str,help="for sort Top-Level Domain")
+args = parser.parse_args()
+
+if args.dork == None:
+    dork = 'inurl:"security.txt" ext:txt -github -wikipedia'
+    if args.tld != None:
+        dork = f"site:{args.tld} " + dork
+else:
+    dork = str(args.dork).replace("'","")
+
+if args.silence == None:
+    print(banner)
+    print(colorama.Fore.YELLOW + "[!] " + dork)
+
+try:
+    os.remove(".google-cookie")
+except:
+    pass
+
+if args.output != None:
+    file = args.output
+    with open(file,'w') as tmp:
+        tmp.close()
+
+count = 0
+try:
+    for results in search(dork, tld="com", lang="en",num=5, start=0, stop=None, pause=2):
+        if args.silence == None:
+            print(colorama.Fore.GREEN +'[+] '+results[results.find('/',results.find('/'))+2:results.find('/',results.find('/')+2)])
+            try:
+                print(colorama.Fore.BLUE + requests.get(results,timeout=2).text+'\n\n')
+            except:
+                pass
+        else:
+            print(colorama.Fore.GREEN + results)
+
+        if args.count != None:
+            if count == args.count:
+                break
+            else:
+                count += 1
+
+        if args.output != None:
+            with open(file,'a') as tmp:
+                tmp.write(results + '\n')
+
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    sys.exit()
+
---
+++ README.md
@@ -0,0 +1,90 @@
+<h1 align="center">
+  Find BBP
+  <br>
+</h1>
+
+---
+
+Find Bug Bounty Program In short Find BBP is a google crawler and tool that discovers bug Bounty Program via security.txt file by default and you can use custom dork
+
+# Features
+
+<h1 align="left">
+  <img src="static/1.png" alt="Find BBP" width="700px"></a>
+  <br>
+</h1>
+
+- Fast and powerful
+- use custom dork
+- output export as txt file
+- sort tld domains
+
+# Installation
+
+Find BBP requires **python3** to install successfully. Run the following command to install:
+
+```sh
+git clone https://github.com/alyrezo/FindBBP
+cd FindBBP
+python -m pip install -r lib.txt
+py FindProgram.py
+```
+
+# Usage
+
+```sh
+py FindProgram.py -h
+```
+This will display help for the tool. Here are all the switches it supports.
+
+```yaml
+usage: FindProgram.py [-h] [--silence] [-d DORK] [-o OUTPUT] [-c COUNT] [-t TLD]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --silence             Not displaying the banner
+  -d , --dork           for using your custom dork
+  -o , --output         result export as txt file
+  -c , --count          count of domains it finds
+  -t , --tld            for sort Top-Level Domain
+
+```
+
+# Running Find BBP
+
+To run the tool, just use the following command.
+
+```sh
+py FindProgram.py 
+```
+<h1 align="left">
+  <img src="static/1.png" alt="Find BBP" width="700px"></a>
+  <br>
+</h1>
+
+```sh
+py FindProgram.py --silence --count 10 --output url.txt
+```
+<h1 align="left">
+  <img src="static/2.png" alt="Find BBP" width="700px"></a>
+  <br>
+</h1>
+
+```sh
+py FindProgram.py --silence --tld io
+```
+<h1 align="left">
+  <img src="static/3.png" alt="Find BBP" width="700px"></a>
+  <br>
+</h1>
+
+```sh
+py FindProgram.py --silence --dork "responsible disclosure program"
+```
+<h1 align="left">
+  <img src="static/4.png" alt="Find BBP" width="700px"></a>
+  <br>
+</h1>
+
+# License
+`Find BBP` is made with 💚 by the [alyrezo](https://Twitter.com/alyrezo)
